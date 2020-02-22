208f0a7c39941895ac36e4f82cd12c492b4f202f
【ritsuyasaito】 <ritsuya.basketball@gmail.com>
2020-02-23 00:17:34 +0900

    addメソッドを追加

---
+++ SampleCal/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SampleCal
+//
+//  Created by 齋藤律哉 on 2020/02/23.
+//  Copyright © 2020 ritsuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var number = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction add() {
+        
+        number += 1
+    }
+
+
+}
+
